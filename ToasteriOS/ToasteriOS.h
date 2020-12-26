c5cf0b0302ccfe93e5e80c79a73a917ebe8502ad
Achsuthan <achsuthan@icloud.com>
2020-12-26 19:07:32 +0800

    Basic files added

---
+++ ToasteriOS/ToasteriOS.h
@@ -0,0 +1,18 @@
+//
+//  ToasteriOS.h
+//  ToasteriOS
+//
+//  Created by achsum on 26/12/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ToasteriOS.
+FOUNDATION_EXPORT double ToasteriOSVersionNumber;
+
+//! Project version string for ToasteriOS.
+FOUNDATION_EXPORT const unsigned char ToasteriOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ToasteriOS/PublicHeader.h>
+
+
---
+++ ToasteriOSExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToasteriOSExamples
+//
+//  Created by achsum on 26/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
