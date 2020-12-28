7d5d0878b0327b41e97a33c321e0b6b8befa3945
Achsuthan <achsuthan@icloud.com>
2020-12-28 12:44:31 +0800

    Sample tested with the app

--- ToasteriOSExamples/ViewController.swift
+++ ToasteriOSExamples/ViewController.swift
@@ -11,7 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.showToaster("Success", "Hello World!", .middle)
+        self.showToaster(title: "Success", message: "Hello World!", position: .middle)
+        self.showToaster(title: "Success", message: "Hello World!", position: .bottom, titleStyle: ToasterMessageStyle(font: UIFont.systemFont(ofSize: 24, weight: .bold), textColor: .red, textAlignment: .center), messageStyle: ToasterMessageStyle(font: UIFont.systemFont(ofSize: 20, weight: .heavy), textColor: .white, textAlignment: .center), toasterBacgroundStyle: ToasterBackgroundStyle(backgroundColor: .black, cornerRadius: 10))
     }
 
 
