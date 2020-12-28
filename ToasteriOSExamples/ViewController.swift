5c246f15fa91189c71510d98d58ea34f5d600063
Achsuthan <achsuthan@icloud.com>
2020-12-28 15:01:54 +0800

    All the option added with sample tableview

--- ToasteriOSExamples/ViewController.swift
+++ ToasteriOSExamples/ViewController.swift
@@ -7,14 +7,90 @@
 
 import UIKit
 
+enum optionEnum {
+    case titleOnly, titleWithStyle, titleWithMessage, titleWithMessageStyle, customToasterBackground, topToaster, MiddleToaster, bottomToaster
+}
+
+struct toasterOptions {
+    var title: String
+    var type: optionEnum
+}
+
 class ViewController: UIViewController {
+    
+    var toastertabel: UITableView = {
+        let tabel = UITableView()
+        tabel.translatesAutoresizingMaskIntoConstraints = false
+        return tabel
+    }()
+    
+    var toasterOptionArray:[toasterOptions] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setUp()
         self.showToaster(title: "Success", message: "Hello World!", position: .middle)
         self.showToaster(title: "Success", message: "Hello World!", position: .bottom, titleStyle: ToasterMessageStyle(font: UIFont.systemFont(ofSize: 24, weight: .bold), textColor: .red, textAlignment: .center), messageStyle: ToasterMessageStyle(font: UIFont.systemFont(ofSize: 20, weight: .heavy), textColor: .white, textAlignment: .center), toasterBacgroundStyle: ToasterBackgroundStyle(backgroundColor: .black, cornerRadius: 10))
     }
+    
+    private func setUp(){
+        
+        self.toasterOptionArray.append(toasterOptions(title: "Title Only", type: .titleOnly))
+        self.toasterOptionArray.append(toasterOptions(title: "Title with style", type: .titleWithStyle))
+        self.toasterOptionArray.append(toasterOptions(title: "Title and Message", type: .titleWithMessage))
+        self.toasterOptionArray.append(toasterOptions(title: "Title and Message with custom style", type: .titleWithMessageStyle))
+        self.toasterOptionArray.append(toasterOptions(title: "Custom Toaster background", type: .customToasterBackground))
+        self.toasterOptionArray.append(toasterOptions(title: "Show Toaster on top", type: .topToaster))
+        self.toasterOptionArray.append(toasterOptions(title: "Show Toaster on middle", type: .MiddleToaster))
+        self.toasterOptionArray.append(toasterOptions(title: "Show Toaster on bottom", type: .bottomToaster))
+        
+        self.view.addSubview(self.toastertabel)
+        self.toastertabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        self.toastertabel.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        self.toastertabel.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        self.toastertabel.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        self.toastertabel.delegate = self
+        self.toastertabel.dataSource = self
+        self.toastertabel.register(ToasterTableViewCell.self, forCellReuseIdentifier: "ToasterTableViewCell")
+    }
 
 
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toasterOptionArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToasterTableViewCell", for: indexPath) as! ToasterTableViewCell
+        cell.selectionStyle = .none
+        cell.lblTitle.text = self.toasterOptionArray[indexPath.row].title
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("selected Index \(indexPath.row)")
+        
+        switch self.toasterOptionArray[indexPath.row].type {
+        case .MiddleToaster:
+            self.showToaster(title: "Success", message: "Hello World!", position: .middle)
+        case .topToaster:
+            self.showToaster(title: "Success", message: "Hello World!", position: .top)
+        case .bottomToaster:
+            self.showToaster(title: "Success", message: "Hello World!", position: .bottom)
+        case .customToasterBackground:
+            self.showToaster(title: "Success", message: "Hello World!", position: .bottom, titleStyle: nil, messageStyle: nil, toasterBacgroundStyle: ToasterBackgroundStyle(backgroundColor: .blue, cornerRadius: 10, delay: 5))
+        case .titleOnly:
+            self.showToaster(title: "Success")
+        case .titleWithMessage:
+            self.showToaster(title: "Success", message: "Hello World!")
+        case .titleWithStyle:
+            self.showToaster(title: "Success", message: nil, position: nil, titleStyle: ToasterMessageStyle(font: UIFont.systemFont(ofSize: 24, weight: .bold), textColor: .green, textAlignment: .center))
+        case .titleWithMessageStyle:
+            self.showToaster(title: "Success", message: "Hello World!", position: nil, titleStyle: ToasterMessageStyle(font: UIFont.systemFont(ofSize: 24, weight: .bold), textColor: .red, textAlignment: .center), messageStyle: ToasterMessageStyle(font: UIFont.systemFont(ofSize: 20, weight: .heavy), textColor: .white, textAlignment: .center))
+        }
+    }
+    
 }
 
