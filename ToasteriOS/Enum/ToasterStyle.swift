f68cb0b93d0d0bb700a1c0dd88fa23caca0155dd
Achsuthan <achsuthan@icloud.com>
2020-12-28 12:23:39 +0800

    1. Toaster background style added 2. Label style added

---
+++ ToasteriOS/Enum/ToasterStyle.swift
@@ -0,0 +1,19 @@
+//
+//  ToasterStyle.swift
+//  ToasteriOS
+//
+//  Created by achsum on 27/12/20.
+//
+
+import UIKit
+
+public struct ToasterMessageStyle {
+    var font: UIFont?
+    var textColor: UIColor?
+    var textAlignment: NSTextAlignment?
+}
+
+public struct ToasterStyle {
+    var backgroundColor: UIColor?
+    var cornerRadius: Double?
+}
