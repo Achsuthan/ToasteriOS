8725be60b0ca4b77b683c2c42a8831f66a4004f9
Achsuthan <achsuthan@icloud.com>
2021-01-02 11:37:19 +0800

    1. Title and message no of line will change based on the text 2. Image added with custom style(image should be square image) 3. Comments added for the functions and variables

--- ToasteriOS/Views/ToasterView.swift
+++ ToasteriOS/Views/ToasterView.swift
@@ -22,6 +22,7 @@ public class ToasterView: UIView {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.textColor = .green
+        label.numberOfLines = 0
         return label
     }()
     
@@ -29,12 +30,23 @@ public class ToasterView: UIView {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.textColor = .white
+        label.numberOfLines = 0
         return label
     }()
     
+    private var toasterImage: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.contentMode = .scaleAspectFill
+        return image
+    }()
+    
+    ////MARK:- Private variable to handle the toaster appearance
     private var vc: UIViewController?
     private var toasterPossion: ToasterPosition = .top
     private var delay: Int = 4
+    private var isShowImage: Bool = false
+    private var toasterSquareImageWidth: Double = 40
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -49,10 +61,12 @@ public class ToasterView: UIView {
             return
         }
         self.setUpToasterView(in: vc)
+        self.setUpToasterImage()
         self.setUpTitle()
         self.setUpMessage()
     }
     
+    //MARK:- Toster background view constrains
     private func setUpToasterView(in vc: UIViewController){
         vc.view.addSubview(self.viewBackground)
         switch self.toasterPossion {
@@ -72,25 +86,50 @@ public class ToasterView: UIView {
         tmp.priority = UILayoutPriority(250)
     }
     
+    //MARK:- Toaster Image constrains
+    private func setUpToasterImage(){
+        self.viewBackground.addSubview(self.toasterImage)
+        if self.isShowImage{
+            self.toasterImage.leadingAnchor.constraint(equalTo: self.viewBackground.leadingAnchor, constant: 10).isActive = true
+            self.toasterImage.heightAnchor.constraint(equalToConstant: CGFloat(self.toasterSquareImageWidth)).isActive = true
+            self.toasterImage.widthAnchor.constraint(equalToConstant: CGFloat(self.toasterSquareImageWidth)).isActive = true
+        }
+        else {
+            self.toasterImage.leadingAnchor.constraint(equalTo: self.viewBackground.leadingAnchor, constant: 0).isActive = true
+            self.toasterImage.widthAnchor.constraint(equalTo: self.viewBackground.heightAnchor, multiplier: 0).isActive = true
+            self.toasterImage.heightAnchor.constraint(equalTo: self.viewBackground.heightAnchor, multiplier: 0).isActive = true
+        }
+        self.toasterImage.centerYAnchor.constraint(equalTo: self.viewBackground.centerYAnchor).isActive = true
+        
+    }
+    
+    //MARK:- Toaster title constrains
     private func setUpTitle(){
         self.viewBackground.addSubview(self.lblTitle)
         self.lblTitle.topAnchor.constraint(equalTo: self.viewBackground.topAnchor, constant: 10).isActive = true
-        self.lblTitle.leadingAnchor.constraint(equalTo: self.viewBackground.leadingAnchor, constant: 10).isActive = true
+        self.lblTitle.leadingAnchor.constraint(equalTo: self.toasterImage.trailingAnchor, constant: 10).isActive = true
         self.lblTitle.trailingAnchor.constraint(equalTo: self.viewBackground.trailingAnchor, constant: -10).isActive = true
         
     }
     
+    //MARK:- Toaster Message constrains
     private func setUpMessage(){
         self.viewBackground.addSubview(self.lblMessage)
         self.lblMessage.topAnchor.constraint(equalTo: self.lblTitle.bottomAnchor, constant: 2).isActive = true
-        self.lblMessage.leadingAnchor.constraint(equalTo: self.viewBackground.leadingAnchor, constant: 10).isActive = true
+        self.lblMessage.leadingAnchor.constraint(equalTo: self.toasterImage.trailingAnchor, constant: 10).isActive = true
         self.lblMessage.trailingAnchor.constraint(equalTo: self.viewBackground.trailingAnchor, constant: -10).isActive = true
         self.lblMessage.bottomAnchor.constraint(equalTo: self.viewBackground.bottomAnchor, constant: -10).isActive = true
     }
     
-    public func showToaster(_ title: String? = "", _ message: String? = "", _ position: ToasterPosition? = .top, vc: UIViewController, _ titleStyle: ToasterMessageStyle? = nil, _ messageStyle: ToasterMessageStyle? = nil, _ toasterBacgroundStyle: ToasterBackgroundStyle? = nil){
+    //MARK:- Show toaster message to user
+    public func showToaster(_ title: String? = "", _ message: String? = "", _ position: ToasterPosition? = .top, vc: UIViewController, _ titleStyle: ToasterMessageStyle? = nil, _ messageStyle: ToasterMessageStyle? = nil, _ toasterBacgroundStyle: ToasterBackgroundStyle? = nil, isWithImage: Bool? = false, toasterImage: UIImage? = nil, toasterSquareImageWidth: Double? = 40){
         self.vc = vc
+        self.toasterSquareImageWidth = toasterSquareImageWidth ?? 40
         self.toasterPossion = position ?? .top
+        if let image = toasterImage {
+            self.toasterImage.image = image
+        }
+        self.isShowImage = isWithImage ?? false
         self.setUp()
         if let title = title {
             self.lblTitle.text = title
